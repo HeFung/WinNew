04c4ee4b188bddd52caf60aa32a0e585c936aacf
Mr. HeFung <qq2236639958@outlook.com>
2025-11-11 13:43:17 +0800

    Update donation.tsx

--- src/page/donation/donation.tsx
+++ src/page/donation/donation.tsx
@@ -14,7 +14,7 @@ export function Donation() {
       marginBottom:'7rem'
     }}>
 
-      <h1 ><strong>捐赠VirtualHotBar</strong></h1>
+      <h1 ><strong>捐赠</strong></h1>
       <br />
 
       <p style={{fontSize: '1rem'}}>WinNew为<strong>非盈利项目</strong>。如果你觉得项目不错请考虑捐赠，以维持项目的持续维护。</p>
@@ -33,13 +33,13 @@ export function Donation() {
         </thead>
         <tbody>
           <tr >
-            <td ><img src="https://p1.hotpe.top/i/PicGo/202302031716843.jpg" /></td>
-            <td ><img src="https://p1.hotpe.top/i/PicGo/202302031715348.jpg" /></td>
-            <td ><img src="https://p1.hotpe.top/i/PicGo/202302031715361.jpg" /></td>
+            <td ><img src="https://cnb.cool/hechangfeng/assets/-/git/raw/main/hefung/wxpay.png" /></td>
+            <td ><img src="https://cnb.cool/hechangfeng/assets/-/git/raw/main/hefung/alipay.png" /></td>
+            <td ><img src="https://cnb.cool/hechangfeng/assets/-/git/raw/main/hefung/qqpay.png" /></td>
           </tr>
         </tbody>
       </table>
-      <h3>感谢您捐赠VirtualHotBar！</h3>
+      <h3>感谢您捐赠！</h3>
 
 
     </div>
